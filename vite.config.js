c0064d4f876c77763a89662110ef4e2988ee8450
LarzzCode <wahidityagialr6@gmail.com>
2025-12-02 18:02:27 +0700

    downgrade tailwind

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
@@ -15,7 +14,7 @@ export default defineConfig({
         description: 'Aplikasi Pencatat Keuangan Pribadi',
         theme_color: '#ffffff',
         background_color: '#ffffff',
-        display: 'standalone', // Ini yang bikin Fullscreen (Address bar hilang)
+        display: 'standalone',
         icons: [
           {
             src: 'pwa-192x192.png',
@@ -31,10 +30,11 @@ export default defineConfig({
             src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable' // Agar icon bulat di Android terlihat bagus
+            purpose: 'any maskable'
           }
         ]
       }
     })
   ],
+  // HAPUS SEMUA KONFIGURASI LAIN DI BAWAH SINI
 })
