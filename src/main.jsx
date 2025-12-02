7ece46169bcaf6c9d51f94279bd52f14768eebde
LarzzCode <wahidityagialr6@gmail.com>
2025-12-02 14:13:16 +0700

    vercel

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-// UBAH DARI BrowserRouter KE HashRouter
-import { HashRouter } from 'react-router-dom' 
+// UBAH KEMBALI KE BrowserRouter
+import { BrowserRouter } from 'react-router-dom' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* Gunakan HashRouter */}
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )
