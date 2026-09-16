845ff96a037f7c0c957a081ed1e979276d3d6a71
Gilar Wahiditya <115379190+LarzzCode@users.noreply.github.com>
2026-09-16 14:04:37 +0700

    chore: keep V3 liquid glass import order

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-// HAPUS IMPORT BROWSERROUTER DARI SINI
-// import { BrowserRouter } from 'react-router-dom' <--- Hapus ini jika ada
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import './liquid-glass.css';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* HAPUS <BrowserRouter> PEMBUNGKUS APP */}
     <App />
-    {/* HAPUS PENUTUP </BrowserRouter> */}
   </React.StrictMode>,
-)
+);
